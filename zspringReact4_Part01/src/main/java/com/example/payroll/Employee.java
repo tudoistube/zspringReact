fc9e9d66cda1f5d4a6ef24bd78ae70f117f65197
tudoistube <tudoistube@ymail.com>
2016-11-14 14:46:41 +0900

    springReact_step01

---
+++ zspringReact4_Part01/src/main/java/com/example/payroll/Employee.java
@@ -0,0 +1,40 @@
+
+package com.example.payroll;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+/**
+ * @author Greg Turnquist
+ */
+
+@Data
+/*
+ * ...@Entity is a JPA annotation that denotes the whole class for storage 
+ *    in a relational table.
+ * ...@Id and @GeneratedValue are JPA annotations to note the primary key 
+ *    and that is generated automatically when needed.
+ * ...@Data is a Project Lombok annotation to autogenerate getters, setters, 
+ *    constructors, toString, hash, equals, and other things. 
+ *    It cuts down on the boilerplate.
+ *    
+ */
+@Entity
+public class Employee {
+
+	private @Id @GeneratedValue Long id;
+	private String firstName;
+	private String lastName;
+	private String description;
+
+	private Employee() {}
+
+	public Employee(String firstName, String lastName, String description) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.description = description;
+	}
+}
